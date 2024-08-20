4b6c1df48916d0d7ff8e3be06bf679e544432370
Bishnu Poudel <bpoudel@DESKTOP-EIS8H6H>
2024-08-20 18:49:50 +0530

    append

--- after-reboot.sh
+++ after-reboot.sh
@@ -14,12 +14,12 @@ check_error() {
 echo "Restarting the pm2 process for the resume app..."
 /home/ubuntu/.nvm/versions/node/v21.7.2/bin/pm2 stop all
 /home/ubuntu/.nvm/versions/node/v21.7.2/bin/pm2 kill
-/home/ubuntu/.nvm/versions/node/v21.7.2/bin/pm2 start npm --name "resume-app" -- start 2>&1 | tee pm2_error.log
+/home/ubuntu/.nvm/versions/node/v21.7.2/bin/pm2 start npm --name "resume-app" -- start 2>&1 | tee -a pm2_error.log
 check_error "Failed to restart the pm2 process for the resume app" "$(cat pm2_error.log)"
 
 # Restart Nginx to apply changes
 echo "Restarting Nginx..."
-sudo systemctl restart nginx 2>&1 | tee nginx_error.log
+sudo systemctl restart nginx 2>&1 | tee -a nginx_error.log
 check_error "Failed to restart Nginx" "$(cat nginx_error.log)"
 
 echo "Deployment successful!"
